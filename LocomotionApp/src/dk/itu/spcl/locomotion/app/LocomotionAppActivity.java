8ef6beb72fb2945378e47de51e54d6b5f865abec
David Thomas <davidat@gmail.com>
2012-04-27 13:00:14 +0200

    initial android app and gson lib

---
+++ LocomotionApp/src/dk/itu/spcl/locomotion/app/LocomotionAppActivity.java
@@ -0,0 +1,17 @@
+package dk.itu.spcl.locomotion.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocomotionAppActivity extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+  
+  
+  
+  
+}
