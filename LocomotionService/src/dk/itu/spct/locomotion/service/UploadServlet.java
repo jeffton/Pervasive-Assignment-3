896f71a73c4cd7fc93580e39187887edf456ede1
Egil Hansen <egil@assimilated.dk>
2012-04-27 14:19:30 +0200

    fixes to service project, changed name

---
+++ LocomotionService/src/dk/itu/spct/locomotion/service/UploadServlet.java
@@ -0,0 +1,39 @@
+package dk.itu.spct.locomotion.service;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.googlecode.objectify.ObjectifyService;
+
+import dk.itu.spct.locomotion.shared.LocomotionData;
+
+@SuppressWarnings("serial")
+public class UploadServlet extends HttpServlet {
+
+	private static final Logger log = Logger.getLogger(UploadServlet.class.getName());
+	
+	static {
+		ObjectifyService.register(LocomotionData.class);
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+	
+		String json = req.getParameter("data");
+		
+		// validate content
+		if(json.isEmpty()) {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "The 'data' argument is empty or missing.");
+			return;
+		}
+		
+		
+	}
+	
+}
